8b8b493ec457d2ec9b3dea5db8a34b6bd68a4713
chris <chris.kind.man@gmail.com>
2022-04-24 15:14:32 +0100

    Add basic highlighting for Manchester Syntax

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use tree_sitter::{Parser, Language, Node};
 use serde_json::{Value};
 use serde_json::json; 
+use tree_sitter_highlight::Highlighter; 
+use tree_sitter_highlight::Highlight; 
+use tree_sitter_highlight::HighlightConfiguration;
 
-extern crate tree_sitter_manchester;
+extern crate tree_sitter_manchester; //this is specified in Cargo.toml
 use tree_sitter_manchester::language;
+use tree_sitter_highlight::HighlightEvent;
 
 //tree transducer for manchester syntax parse tree
 //I distinguish between three cases:
@@ -27,6 +31,7 @@ pub fn translate(n : &Node, raw : &str) -> Value {
          return match n.kind() {
              "primaryNegation" => translate_operator(&n.named_child(0).unwrap(), raw, "ComplementOf"),
              "restrictionNegation" => translate_operator(&n.named_child(0).unwrap(), raw, "ComplementOf"),
+             "inverseObjectProperty" => translate_operator(&n.named_child(0).unwrap(), raw, "InverseOf"),
              "objectPropertySelf" => translate_operator(&n, raw, "HasSelf"), 
              _ => translate(&n.named_child(0).unwrap(), raw),//default for (i)
          }
@@ -52,6 +57,7 @@ pub fn translate(n : &Node, raw : &str) -> Value {
     }
 }
 
+//this gets atomic entities (labels,entity names,etc.)
 pub fn translate_raw(n : &Node, raw : &str) -> Value { 
     let start = n.start_position().column;
     let end = n.end_position().column;
@@ -70,27 +76,165 @@ pub fn translate_operator(n : &Node, raw : &str, operator : &str) -> Value {
     json!(res)
 }
 
+#[derive(Debug)]
+pub struct HighlightSlice {
+    syntax: String,
+    highlight: String, 
+}
+
+pub fn get_highlight_code(h:  Highlight) -> usize { 
+        match h {
+            Highlight(x) => x,
+        }
+}
+
+pub fn highlight(input: &str) -> String  {
+    let mut highlighter = Highlighter::new(); 
+
+    let mut man_config = HighlightConfiguration::new(
+        tree_sitter_manchester::language(),
+        tree_sitter_manchester::HIGHLIGHTS_QUERY,
+        "",
+        "",
+        ).unwrap();
+
+    let highlight_names = &[
+    "blue",
+    "magenta",
+    "turquise",
+    "white",
+    "gray",
+    ];
+
+    man_config.configure(&highlight_names[..]);
+    let highlights = highlighter.highlight(
+        &man_config,
+        input.as_bytes(),
+        None,
+        |_| None
+        ).unwrap();
+
+    let mut res = Vec::new(); 
+    let h = HighlightSlice{syntax:String::from("a"),
+    highlight: String::from("b"), };
+    res.push(h);
+    println!("{:?}", res);
+
+    //TODO: implement a stack to iterate thourhg this
+    let mut stack = Vec::new();
+
+    println!("Highliting start");
+    for event in highlights {
+        match event.unwrap() {
+            HighlightEvent::Source {start, end} => {
+
+                //let hh: usize = end;
+                let hh: &str = &input[start..end];
+                eprintln!("source: {}-{}", start, end);
+                eprintln!("source: {:?}", &input[start..end]); 
+                if ! stack.is_empty() {
+                    println!("ah {:?}", stack[0]);
+                } else {
+                    println!("bh"); 
+                }
+
+    //let extract = &raw[start..end];
+    //json!(extract) 
+            },
+            HighlightEvent::HighlightStart(s) => {
+                eprintln!("highlight style started: {:?}", s);
+                eprintln!("highlight style started: {:?}", highlight_names[get_highlight_code(s)]);
+                stack.push(get_highlight_code(s));
+            },
+            HighlightEvent::HighlightEnd => {
+                eprintln!("highlight style ended");
+                stack.pop();
+            },
+        }
+    }
+    println!("Highliting end");
+
+    String::from("asd")
+}
+
 fn main() {
+    let mut highlighter = Highlighter::new(); 
     //let code = "((b or c) and d) and e"; 
     //let code = "a or bbaba and ddd"; 
     //let code = "not((not a) and b)";
-    //let code = "a some p or b only d";
+    //let code = "g SubClassOf: inverse l some not p or b only d";
     //let code = "a exactly 2 asd";
     //let code = "a some asd";
     //let code = "a exactly 2 asd or b some d:asdA";
-    //let code = "(aa some (b or c)) and (p some d)";
+    //let code = "g SubClassOf: (aa some (b or c)) and (p some d)";
     //let code = "(bo1 some (obo2 or obo3)) and (obo4 some obo5)";
     //let code = "oboOBI_0000293 some (oboIAO_0000010 or oboIAO_0000096)";
     //let code = "obo:OBI_0000293 some (obo:IAO_0000010 or obo:IAO_0000096)";
      //let code = "(obo:OBI_0000293 some (obo:IAO_0000010 or obo:IAO_0000096)) and (obo:OBI_0000299 some obo:IAO_0000010)";
     //let code = "obo:OBI_0001875 and (obo:OBI_0000643 some (obo:CL_0000000 and (not (obo:BFO_0000051 some obo:OBI_1110132)) and (not (obo:BFO_0000051 some obo:PR_000001004))))";
+    //
+    let code = "obo:OBI_0000070 SubClassOf: obo:OBI_0000299 some (obo:IAO_0000027 and (obo:IAO_0000136 only (obo:BFO_0000040 or (not (obo:RO_0000087 some obo:OBI_0000067)))))";
 
     //let code = "test DisjointWith Student or Person that hasAge some number";
-    let code = "test DisjointUnionOf {a, b}";
+    //let code = "test DisjointUnionOf a, b";
+    //let code = "obo:OBI_0000070 DisjointWith: obo:OBI_0000339, obo:OBI_0200000, obo:OBI_0600013";
+    //let code = "obo:IAO_0000078 EquivalentTo: obo:IAO_0000002 , obo:IAO_0000120 , obo:IAO_0000121 , obo:IAO_0000122 , obo:IAO_0000123 , obo:IAO_0000124 , obo:IAO_0000125 , obo:IAO_0000423 , obo:IAO_0000428";
 
     let mut parser = Parser::new();
 
     let language : Language = language();
+
+    let mut man_config = HighlightConfiguration::new(
+        tree_sitter_manchester::language(),
+        tree_sitter_manchester::HIGHLIGHTS_QUERY,
+        "",
+        "",
+        ).unwrap();
+
+    let highlight_names = &[
+    "blue",
+    "magenta",
+    "turquise",
+    "white",
+    "gray",
+    ];
+
+    man_config.configure(&highlight_names[..]);
+
+        //let example_string = b"obo:OBI_0000070 SubClassOf: obo:OBI_0000299 some (obo:IAO_0000027 and (obo:IAO_0000136 only (obo:BFO_0000040 or (not (obo:RO_0000087 some obo:OBI_0000067)))))";
+        let example_string_2 = "obo:OBI_0000070 SubClassOf: obo:OBI_0000299 some (obo:IAO_0000027 and (obo:IAO_0000136 only (obo:BFO_0000040 or (not (obo:RO_0000087 some obo:OBI_0000067)))))";
+
+    let highlights = highlighter.highlight(
+        &man_config,
+        //b"obo:OBI_0000070 SubClassOf: obo:OBI_0000299 some (obo:IAO_0000027 and (obo:IAO_0000136 only (obo:BFO_0000040 or (not (obo:RO_0000087 some obo:OBI_0000067)))))",
+        example_string_2.as_bytes(),
+        None,
+        |_| None
+        ).unwrap();
+
+    println!("Highliting start");
+    for event in highlights {
+        match event.unwrap() {
+            HighlightEvent::Source {start, end} => {
+                eprintln!("source: {}-{}", start, end);
+                eprintln!("source: {:?}", &example_string_2[start..end]);
+
+    //let extract = &raw[start..end];
+    //json!(extract) 
+            },
+            HighlightEvent::HighlightStart(s) => {
+                eprintln!("highlight style started: {:?}", s);
+                eprintln!("highlight style started: {:?}", highlight_names[get_highlight_code(s)]);
+            },
+            HighlightEvent::HighlightEnd => {
+                eprintln!("highlight style ended");
+            },
+        }
+    }
+    println!("Highliting end");
+
+
+
     parser.set_language(language).expect("Error loading manchester grammar");
     let tree = parser.parse(code, None).unwrap();
 
@@ -105,6 +249,9 @@ fn main() {
     println!("Translation: {:#?}", t);
     println!("");
     println!("Serialisation: {:?}", serde_json::to_string(&t).unwrap());
+    println!("Serialisation: {}", serde_json::to_string(&t).unwrap());
+
+    highlight("obo:OBI_0000070 SubClassOf: obo:OBI_0000299 some (obo:IAO_0000027 and (obo:IAO_0000136 only (obo:BFO_0000040 or (not (obo:RO_0000087 some obo:OBI_0000067)))))");
 
 
     //let mut rc = tree.root_node().child(0).unwrap().walk();
